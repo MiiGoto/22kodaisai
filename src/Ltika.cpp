6f5f2b0fdb21d5f8605f56db0a092aba61731257
yoshiko_kulala <kulala.mbili@gmail.com>
2022-10-17 05:49:32 +0000

    for Omen

--- src/Ltika.cpp
+++ src/Ltika.cpp
@@ -3,6 +3,8 @@
 
 #include <std_msgs/Bool.h>
 
+int pi;
+
 void cb_LED(const std_msgs::Bool::ConstPtr &data){
   if(data->data == 1){
     gpio_write(pi,26,1);
@@ -13,7 +15,7 @@ void cb_LED(const std_msgs::Bool::ConstPtr &data){
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "gpio");
-  int pi = pigpio_start(NULL,NULL);
+  pi = pigpio_start(NULL,NULL);
   ros::NodeHandle nh;
   ros::Subscriber sub_led=nh.subscribe("/led", 5, cb_LED);
   ros::spin();
