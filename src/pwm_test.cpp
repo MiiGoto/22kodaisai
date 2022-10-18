b32b92a3f61f854382ce729147123201c9764d38
yoshiko_kulala <kulala.mbili@gmail.com>
2022-10-18 06:25:58 +0000

    add pwm

--- src/pwm_test.cpp
+++ src/pwm_test.cpp
@@ -2,13 +2,14 @@
 #include <pigpiod_if2.h>
 
 #include <std_msgs/Bool.h>
-int pi;
+int pi,duty=0;
 
 void cb_LED(const std_msgs::Bool::ConstPtr &data){
   if(data->data == 1){
-    gpio_write(pi,26,1);
+    //gpio_write(pi,26,1);
+    set_PWM_dutycycle(pi,26,125);
   }else{
-    gpio_write(pi,26,0);
+    set_PWM_dutycycle(pi,26,0);
   }
 }
 
@@ -16,6 +17,8 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "gpio");
   pi = pigpio_start(NULL,NULL);
   ros::NodeHandle nh;
+  set_mode(pi, 26, PI_OUTPUT);
+  set_PWM_frequency(pi,26,1000);
   ros::Subscriber sub_led=nh.subscribe("/led", 5, cb_LED);
   ros::spin();
   return 0;
